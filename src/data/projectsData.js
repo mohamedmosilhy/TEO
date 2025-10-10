19614b785a720961baff816f2455ba5cb841ca38
mohamed mosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2025-10-10 21:59:11 +0300

    Remove unused project images from various residential designs, including Paint House Diar and Paint House Mountain View, and update projectsData.js to utilize dynamic image loading for improved maintainability.

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,262 +1,137 @@
-import project2_1 from "../assets/images/projects/residential/design/i-villa-abha-compound/1.jpeg";
-import project2_2 from "../assets/images/projects/residential/design/i-villa-abha-compound/2.jpeg";
-import project2_3 from "../assets/images/projects/residential/design/i-villa-abha-compound/3.jpeg";
-import project2_4 from "../assets/images/projects/residential/design/i-villa-abha-compound/4.jpeg";
-import project2_5 from "../assets/images/projects/residential/design/i-villa-abha-compound/5.jpeg";
-import project2_6 from "../assets/images/projects/residential/design/i-villa-abha-compound/6.jpg";
-import project2_7 from "../assets/images/projects/residential/design/i-villa-abha-compound/7.jpg";
-import project2_8 from "../assets/images/projects/residential/design/i-villa-abha-compound/8.jpg";
-import project2_9 from "../assets/images/projects/residential/design/i-villa-abha-compound/9.jpg";
-import project2_10 from "../assets/images/projects/residential/design/i-villa-abha-compound/10.jpg";
-import project2_11 from "../assets/images/projects/residential/design/i-villa-abha-compound/11.jpg";
-import project2_12 from "../assets/images/projects/residential/design/i-villa-abha-compound/12.jpg";
-import project2_13 from "../assets/images/projects/residential/design/i-villa-abha-compound/13.jpg";
-import project2_14 from "../assets/images/projects/residential/design/i-villa-abha-compound/14.jpg";
+// src/data/projectsData.js
 
-import project4_1 from "../assets/images/projects/residential/design/paint-house-mountain-view/Girl Room 1.jpg";
-import project4_2 from "../assets/images/projects/residential/design/paint-house-mountain-view/Girl Room 2.jpg";
-import project4_3 from "../assets/images/projects/residential/design/paint-house-mountain-view/Girl Room 3.jpg";
-import project4_4 from "../assets/images/projects/residential/design/paint-house-mountain-view/Girl Room 4.jpg";
-import project4_5 from "../assets/images/projects/residential/design/paint-house-mountain-view/Girl Room 5.jpg";
-import project4_6 from "../assets/images/projects/residential/design/paint-house-mountain-view/Guest Bath 1.jpg";
-import project4_7 from "../assets/images/projects/residential/design/paint-house-mountain-view/Guest Bath 2.jpg";
-import project4_8 from "../assets/images/projects/residential/design/paint-house-mountain-view/Guest Bath 3.jpg";
-import project4_9 from "../assets/images/projects/residential/design/paint-house-mountain-view/Kids Room 1.jpg";
-import project4_10 from "../assets/images/projects/residential/design/paint-house-mountain-view/Kids Room 2.jpg";
-import project4_11 from "../assets/images/projects/residential/design/paint-house-mountain-view/Kids Room 3.jpg";
-import project4_12 from "../assets/images/projects/residential/design/paint-house-mountain-view/Kids Room 4.jpg";
-import project4_13 from "../assets/images/projects/residential/design/paint-house-mountain-view/Kids Room 5.jpg";
-import project4_14 from "../assets/images/projects/residential/design/paint-house-mountain-view/Kitchen 1.jpg";
-import project4_15 from "../assets/images/projects/residential/design/paint-house-mountain-view/Kitchen 2.jpg";
-import project4_16 from "../assets/images/projects/residential/design/paint-house-mountain-view/Kitchen 3.jpg";
-import project4_17 from "../assets/images/projects/residential/design/paint-house-mountain-view/Kitchen 4.jpg";
-import project4_18 from "../assets/images/projects/residential/design/paint-house-mountain-view/Living Room 1.jpg";
-import project4_19 from "../assets/images/projects/residential/design/paint-house-mountain-view/Living Room 2.jpg";
-import project4_20 from "../assets/images/projects/residential/design/paint-house-mountain-view/Living Room 3.jpg";
-import project4_21 from "../assets/images/projects/residential/design/paint-house-mountain-view/Living Room 4.jpg";
-import project4_22 from "../assets/images/projects/residential/design/paint-house-mountain-view/Main Bath 1.jpg";
-import project4_23 from "../assets/images/projects/residential/design/paint-house-mountain-view/Main Bath 2.jpg";
-import project4_24 from "../assets/images/projects/residential/design/paint-house-mountain-view/Main Bath 3.jpg";
-import project4_25 from "../assets/images/projects/residential/design/paint-house-mountain-view/Master Bath 1.jpg";
-import project4_26 from "../assets/images/projects/residential/design/paint-house-mountain-view/Master Bath 2.jpg";
-import project4_27 from "../assets/images/projects/residential/design/paint-house-mountain-view/Master Bath 3.jpg";
-import project4_28 from "../assets/images/projects/residential/design/paint-house-mountain-view/Master Room 1.jpg";
-import project4_29 from "../assets/images/projects/residential/design/paint-house-mountain-view/Master Room 2.jpg";
-import project4_30 from "../assets/images/projects/residential/design/paint-house-mountain-view/Master Room 3.jpg";
-import project4_31 from "../assets/images/projects/residential/design/paint-house-mountain-view/Master Room 4.jpg";
-import project4_32 from "../assets/images/projects/residential/design/paint-house-mountain-view/Master Room 5.jpg";
-import project4_33 from "../assets/images/projects/residential/design/paint-house-mountain-view/Master Room 6.jpg";
-import project4_34 from "../assets/images/projects/residential/design/paint-house-mountain-view/Office 1.jpg";
-import project4_35 from "../assets/images/projects/residential/design/paint-house-mountain-view/Office 2.jpg";
+// Helper: sort image keys before mapping
+function sortAndMap(images) {
+  return Object.keys(images)
+    .sort((a, b) => {
+      const numA = parseInt(a.match(/(\d+)/)?.[0] || 0, 10);
+      const numB = parseInt(b.match(/(\d+)/)?.[0] || 0, 10);
+      return numA - numB || a.localeCompare(b);
+    })
+    .map((key) => images[key].default);
+}
 
-import project1_1 from "../assets/images/projects/residential/design/town-house-karma-gates/1.jpg";
-import project1_2 from "../assets/images/projects/residential/design/town-house-karma-gates/0.jpg";
-import project1_3 from "../assets/images/projects/residential/design/town-house-karma-gates/-1.jpg";
-import project1_4 from "../assets/images/projects/residential/design/town-house-karma-gates/2.jpg";
-import project1_5 from "../assets/images/projects/residential/design/town-house-karma-gates/3.jpg";
-import project1_6 from "../assets/images/projects/residential/design/town-house-karma-gates/4.jpg";
-import project1_7 from "../assets/images/projects/residential/design/town-house-karma-gates/5.jpg";
-import project1_8 from "../assets/images/projects/residential/design/town-house-karma-gates/6.jpg";
-import project1_9 from "../assets/images/projects/residential/design/town-house-karma-gates/7.jpg";
-import project1_10 from "../assets/images/projects/residential/design/town-house-karma-gates/8.jpg";
-import project1_11 from "../assets/images/projects/residential/design/town-house-karma-gates/9.jpg";
-import project1_12 from "../assets/images/projects/residential/design/town-house-karma-gates/10.jpg";
-import project1_13 from "../assets/images/projects/residential/design/town-house-karma-gates/010.jpg";
-import project1_14 from "../assets/images/projects/residential/design/town-house-karma-gates/11.jpeg";
-import project1_15 from "../assets/images/projects/residential/design/town-house-karma-gates/12.jpeg";
-import project1_16 from "../assets/images/projects/residential/design/town-house-karma-gates/13.jpeg";
-import project1_17 from "../assets/images/projects/residential/design/town-house-karma-gates/14.jpg";
-import project1_18 from "../assets/images/projects/residential/design/town-house-karma-gates/15.jpg";
-import project1_19 from "../assets/images/projects/residential/design/town-house-karma-gates/16.jpg";
-import project1_20 from "../assets/images/projects/residential/design/town-house-karma-gates/17.jpg";
-import project1_21 from "../assets/images/projects/residential/design/town-house-karma-gates/18.jpeg";
-import project1_22 from "../assets/images/projects/residential/design/town-house-karma-gates/19.jpeg";
-import project1_23 from "../assets/images/projects/residential/design/town-house-karma-gates/20.jpeg";
+// Auto-load + sort all project images
+const project1Images = sortAndMap(
+  import.meta.glob(
+    "../assets/images/projects/residential/design/town-house-karma-gates/*.{jpg,jpeg,png}",
+    { eager: true }
+  )
+);
 
-import project3_1 from "../assets/images/projects/residential/design/paint-house-diar/1.jpeg";
-import project3_2 from "../assets/images/projects/residential/design/paint-house-diar/2.jpeg";
-import project3_3 from "../assets/images/projects/residential/design/paint-house-diar/3.jpeg";
-import project3_4 from "../assets/images/projects/residential/design/paint-house-diar/4.jpeg";
-import project3_5 from "../assets/images/projects/residential/design/paint-house-diar/5.jpeg";
-import project3_6 from "../assets/images/projects/residential/design/paint-house-diar/6.jpeg";
-import project3_7 from "../assets/images/projects/residential/design/paint-house-diar/7.jpeg";
-import project3_8 from "../assets/images/projects/residential/design/paint-house-diar/8.jpg";
-import project3_9 from "../assets/images/projects/residential/design/paint-house-diar/9.jpg";
-import project3_10 from "../assets/images/projects/residential/design/paint-house-diar/10.jpg";
-import project3_11 from "../assets/images/projects/residential/design/paint-house-diar/11.jpg";
+const project2Images = sortAndMap(
+  import.meta.glob(
+    "../assets/images/projects/residential/design/i-villa-abha-compound/*.{jpg,jpeg,png}",
+    { eager: true }
+  )
+);
 
-import project6_1 from "../assets/images/projects/landscaping/design/PROJECT1/Roof Indoor 1.jpg";
-import project6_2 from "../assets/images/projects/landscaping/design/PROJECT1/Roof Indoor 2.jpg";
-import project6_3 from "../assets/images/projects/landscaping/design/PROJECT1/Roof Indoor 3.jpg";
-import project6_4 from "../assets/images/projects/landscaping/design/PROJECT1/Roof Outdoor 1.jpg";
-import project6_5 from "../assets/images/projects/landscaping/design/PROJECT1/Roof Outdoor 2.jpg";
-import project6_6 from "../assets/images/projects/landscaping/design/PROJECT1/Roof Outdoor 3.jpg";
-import project6_7 from "../assets/images/projects/landscaping/design/PROJECT1/Roof Outdoor 4.jpg";
-import project6_8 from "../assets/images/projects/landscaping/design/PROJECT1/Roof Outdoor 5.jpg";
-import project6_9 from "../assets/images/projects/landscaping/design/PROJECT1/Roof Outdoor 6.jpg";
-import project6_10 from "../assets/images/projects/landscaping/design/PROJECT1/Roof Outdoor 7.jpg";
+const project4Images = sortAndMap(
+  import.meta.glob(
+    "../assets/images/projects/residential/design/paint-house-el-patio-oro/*.{jpg,jpeg,png}",
+    { eager: true }
+  )
+);
 
-import project7_1 from "../assets/images/projects/landscaping/design/PROJECT2/5.jpg";
-import project7_2 from "../assets/images/projects/landscaping/design/PROJECT2/22.jpg";
+const project6Images = sortAndMap(
+  import.meta.glob(
+    "../assets/images/projects/landscaping/design/PROJECT1/*.{jpg,jpeg,png}",
+    { eager: true }
+  )
+);
 
-import project8_1 from "../assets/images/projects/landscaping/design/PROJECT3/1.jpg";
-import project8_2 from "../assets/images/projects/landscaping/design/PROJECT3/2.jpg";
+const project7Images = sortAndMap(
+  import.meta.glob(
+    "../assets/images/projects/landscaping/design/PROJECT2/*.{jpg,jpeg,png}",
+    { eager: true }
+  )
+);
+
+const project8Images = sortAndMap(
+  import.meta.glob(
+    "../assets/images/projects/landscaping/design/PROJECT3/*.{jpg,jpeg,png}",
+    { eager: true }
+  )
+);
+
+const project9Images = sortAndMap(
+  import.meta.glob(
+    "../assets/images/projects/landscaping/design/Villa palm valley/*.{jpg,jpeg,png}",
+    { eager: true }
+  )
+);
+
+const project10Images = sortAndMap(
+  import.meta.glob(
+    "../assets/images/projects/residential/design/Villa in PATIO EL ZAHRAA/*.{jpg,jpeg,png}",
+    { eager: true }
+  )
+);
 
 export const projects = [
   {
     id: 1,
-    img: project1_1,
+    img: project1Images[0],
     title: "TOWN HOUSE KARMA GATES",
     category: "Residential",
     type: "Design",
-    images: [
-      project1_1,
-      project1_2,
-      project1_3,
-      project1_4,
-      project1_5,
-      project1_6,
-      project1_7,
-      project1_8,
-      project1_9,
-      project1_10,
-      project1_11,
-      project1_12,
-      project1_13,
-      project1_14,
-      project1_15,
-      project1_16,
-      project1_17,
-      project1_18,
-      project1_19,
-      project1_20,
-      project1_21,
-      project1_22,
-      project1_23,
-    ],
+    images: project1Images,
   },
   {
     id: 2,
-    img: project2_1,
+    img: project2Images[0],
     title: "I VILLA - ABHA COMPOUND",
     category: "Residential",
     type: "Design",
-    images: [
-      project2_1,
-      project2_2,
-      project2_3,
-      project2_4,
-      project2_5,
-      project2_6,
-      project2_7,
-      project2_8,
-      project2_9,
-      project2_10,
-      project2_11,
-      project2_12,
-      project2_13,
-      project2_14,
-    ],
+    images: project2Images,
   },
-
   {
     id: 3,
-    img: project3_1,
-    title: "PAINT HOUSE DIAR",
+    img: project4Images[0],
+    title: "PAINT HOUSE - El-Patio Oro",
     category: "Residential",
     type: "Design",
-    images: [
-      project3_1,
-      project3_2,
-      project3_3,
-      project3_4,
-      project3_5,
-      project3_6,
-      project3_7,
-      project3_8,
-      project3_9,
-      project3_10,
-      project3_11,
-    ],
+    images: project4Images,
   },
   {
     id: 4,
-    img: project4_1,
-    title: "PAINT HOUSE - MOUNTAIN VIEW",
-    category: "Residential",
-    type: "Design",
-    images: [
-      project4_1,
-      project4_2,
-      project4_3,
-      project4_4,
-      project4_5,
-      project4_6,
-      project4_7,
-      project4_8,
-      project4_9,
-      project4_10,
-      project4_11,
-      project4_12,
-      project4_13,
-      project4_14,
-      project4_15,
-      project4_16,
-      project4_17,
-      project4_18,
-      project4_19,
-      project4_20,
-      project4_21,
-      project4_22,
-      project4_23,
-      project4_24,
-      project4_25,
-      project4_26,
-      project4_27,
-      project4_28,
-      project4_29,
-      project4_30,
-      project4_31,
-      project4_32,
-      project4_33,
-      project4_34,
-      project4_35,
-    ],
-  },
-  {
-    id: 5,
-    img: project6_1,
+    img: project6Images[0],
     title: "PROJECT 1",
     category: "Landscaping",
     type: "Design",
-    images: [
-      project6_1,
-      project6_2,
-      project6_3,
-      project6_4,
-      project6_5,
-      project6_6,
-      project6_7,
-      project6_8,
-      project6_9,
-      project6_10,
-    ],
+    images: project6Images,
   },
   {
-    id: 6,
-    img: project7_1,
+    id: 5,
+    img: project7Images[0],
     title: "PROJECT 2",
     category: "Landscaping",
     type: "Design",
-    images: [project7_1, project7_2],
+    images: project7Images,
   },
   {
-    id: 7,
-    img: project8_1,
+    id: 6,
+    img: project8Images[0],
     title: "PROJECT 3",
     category: "Landscaping",
     type: "Design",
-    images: [project8_1, project8_2],
+    images: project8Images,
+  },
+  {
+    id: 7,
+    img: project9Images[0],
+    title: "Villa palm valley",
+    category: "Landscaping",
+    type: "Design",
+    images: project9Images,
+  },
+  {
+    id: 8,
+    img: project10Images[0],
+    title: "Villa in PATIO EL ZAHRAA",
+    category: "Residential",
+    type: "Design",
+    images: project10Images,
   },
 ];
 
